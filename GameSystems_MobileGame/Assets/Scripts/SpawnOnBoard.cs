498af3f590e200a835f50acdb476f20dec4b9ec9
AndyJL1999 <andylainez115@hotmail.com>
2022-02-23 18:05:51 -0500

    LineRender follows player with corresponding color.

--- GameSystems_MobileGame/Assets/Scripts/SpawnOnBoard.cs
+++ GameSystems_MobileGame/Assets/Scripts/SpawnOnBoard.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class SpawnOnBoard : MonoBehaviour
 {
     public GameObject[] players;
+    public LineRenderer[] lines;
+    public GameObject winnerPanel;
+    public GameObject winner;
+    public Button playButton;
     public Text roundText;
+    public Text winningText;
     private int numOfPlayers;
 
     void Start()
     {
         Time.timeScale = 1;
+        playButton.enabled = true;
+        winnerPanel.SetActive(false);
         numOfPlayers = GameManager.playerNum;
         roundText.text = "Round " + GameManager.roundNum;
         SetBoard();
@@ -23,6 +30,15 @@ public class SpawnOnBoard : MonoBehaviour
         {
             if(GameManager.movePlayers[i].y > 0)
                 players[i].transform.position = Vector2.Lerp(players[i].transform.position, GameManager.movePlayers[i], Time.deltaTime);
+
+            lines[i].SetPosition(i, new Vector2(players[i].transform.position.x, players[i].transform.position.y));
+            lines[i].startColor = players[i].GetComponent<SpriteRenderer>().color;
+            lines[i].endColor = players[i].GetComponent<SpriteRenderer>().color;
+
+            if (players[i].transform.position.y >= 10)
+            {
+                SetWinnerPanel(i);
+            }
         }
        
     }
@@ -42,6 +58,14 @@ public class SpawnOnBoard : MonoBehaviour
             players[i].GetComponent<SpriteRenderer>().sprite = GameManager.sprites[i];
             players[i].GetComponent<SpriteRenderer>().color = GameManager.colors[i];
         }
+    }
+
+    void SetWinnerPanel(int playerIndex)
+    {
+        winnerPanel.SetActive(true);
+        winningText.text = $"Player {playerIndex + 1} wins!";
+        winner.GetComponent<SpriteRenderer>().sprite = players[playerIndex].GetComponent<SpriteRenderer>().sprite;
+        playButton.enabled = false;
     }
 
     public void ResetBoard()
